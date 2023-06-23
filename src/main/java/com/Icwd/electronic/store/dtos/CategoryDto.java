7d72875d4f29c4dc6daa6a2191883c4db054fd47
Ashutosh Dongare <ashudongare07@gmail.com>
2023-06-23 20:03:01 +0530

    created category module

---
+++ src/main/java/com/Icwd/electronic/store/dtos/CategoryDto.java
@@ -0,0 +1,31 @@
+package com.Icwd.electronic.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CategoryDto {
+
+
+    private String categoryId;
+
+    @NotBlank
+    @Min(value = 4,message = "title must be of min 4 characters")
+    private String title;
+
+    @NotBlank(message = "Description required!")
+    private String description;
+
+    private String coverImage;
+}
---
+++ src/main/java/com/Icwd/electronic/store/entities/Category.java
@@ -0,0 +1,34 @@
+package com.Icwd.electronic.store.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="categories")
+public class Category {
+
+    @Id
+    @Column(name = "id")
+    private String categoryId;
+
+    @Column(name = "category_title",length = 60,nullable = false)
+    private String title;
+
+    @Column(name = "category_desc",length = 50)
+    private String description;
+
+    private String coverImage;
+
+}
