e6ccbb6fee4018e5059ff14edb71a353c327b0d3
Ashutosh Dongare <ashudongare07@gmail.com>
2023-06-25 01:46:37 +0530

    created ProductDto.java class

---
+++ src/main/java/com/Icwd/electronic/store/dtos/ProductDto.java
@@ -0,0 +1,25 @@
+package com.Icwd.electronic.store.dtos;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ProductDto {
+
+    private String productId;
+    private String title;
+    private String description;
+    private int price;
+    private int discountedPrice;
+    private int quantity;
+    private Date addedDate;
+    private boolean live;
+    private boolean stock;
+}
